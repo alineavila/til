ee3d807457981daffaa75f23977043b296286c01
alineavila <aline.avila01@gmail.com>
2020-03-22 21:40:58 -0300

    default constructor

--- JPA/aprendendojpa/src/main/java/com/aline/aprendendojpa/model/Cliente.java
+++ JPA/aprendendojpa/src/main/java/com/aline/aprendendojpa/model/Cliente.java
@@ -12,14 +12,27 @@ import javax.persistence.Table;
 @Table (name="cliente")
 public class Cliente {
 
+    public Cliente() { // JPA espera um construtor default sem parâmetros
+
+    }
+
+    public Cliente(long id, String nome) {
+        this.id = id;
+        this.nome = nome;
+    }
+
     @Id
     @Column(name="id")
-    private Integer id;
+    private long id;
 
     @Column(name="nome")
     private String nome;
 
-
-
-
+    @Override
+    public String toString() {
+        return "Cliente{" +
+                "id=" + id +
+                ", nome='" + nome + '\'' +
+                '}';
+    }
 }
