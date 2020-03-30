dc44adccc0d8e33b44a70eb6fde20917b9ec2e61
alineavila <aline.avila01@gmail.com>
2020-03-30 03:57:22 -0300

    feito consulta com filtros, relacionamentos, jpql, @param entre outros

--- ALURA/Spring Boot Parte 1 - API REST/forum/src/main/java/br/com/alura/forum/repository/TopicoRepository.java
+++ ALURA/Spring Boot Parte 1 - API REST/forum/src/main/java/br/com/alura/forum/repository/TopicoRepository.java
@@ -1,5 +1,7 @@
 package br.com.alura.forum.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import br.com.alura.forum.model.Topico;
@@ -8,4 +10,6 @@ import br.com.alura.forum.model.Topico;
 // vamos herdar de alguma interface do jpa
 public interface TopicoRepository extends JpaRepository<Topico, Long> {
 
+	List<Topico> findByCursoNome(String nomeCurso); // curso é a entidade de relacionamento, nome é o atributo que eu quero achar dessa entidade
+	// o SpringData já sabe que é pra fazer os joins  
 }
